3d45b9f2df07d7d2f83dcaf45c9caf565cd9a8a9
bigyihsuan <bigyihsuan@gmail.com>
2022-01-18 20:17:09 -0500

    chapter 4 "interactive prompt"

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello world!\n";
+    std::cout << "cpp-lispy v0.0.0-1\n"
+              << "Press ctrl-c to exit\n";
+
+    while (true)
+    {
+        std::string input;
+
+        std::cout << "lispy> ";
+        std::getline(std::cin, input);
+        std::printf("No you're a %s\n", input.c_str());
+    }
+
+    return 0;
 }
