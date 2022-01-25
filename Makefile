bfaf92e1f7febf49074aa019a99523e98dc87a2b
bigyihsuan <bigyihsuan@gmail.com>
2022-01-25 17:10:58 -0500

    use `new` to keep lexer and parser alive

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ lispy_grammar = Lispy.g4
 
 lispy: $(parsing) src/$(lispy_grammar)
 	cd src;	$(antlr4) -Dlanguage=Cpp -o $(ANTLR_DIR) $(lispy_grammar); cd ..
-	g++ -g -std=c++11 -Wall $(Iantlr4) -I src/antlr -o lispy \
+	g++ -g -std=c++2a -Wall $(Iantlr4) -I src/antlr -o lispy \
 		$(parsing) src/antlr/*.cpp $(ledit) $(lantlr4)
 
 clean:
--- src/parsing.cpp
+++ src/parsing.cpp
@@ -1,41 +1,42 @@
-#include <iostream>
-#include <string>
+#include <antlr4-runtime/antlr4-runtime.h>
 #include <editline/history.h>
 #include <editline/readline.h>
 
-#include <antlr4-runtime/antlr4-runtime.h>
+#include <iostream>
+#include <string>
+
 #include "./antlr/LispyLexer.h"
 #include "./antlr/LispyParser.h"
 
 int main()
 {
+    std::cout << "cpp-lispy v0.0.0\n"
+              << "Press ctrl-c to exit\n";
 
-    std::cout
-        << "cpp-lispy v0.0.0\n"
-        << "Press ctrl-c to exit\n";
-
-    // while (true)
-    // {
-    std::string input;
-    std::cout << "lispy> ";
-    std::getline(std::cin, input);
-    add_history(input.c_str());
-
-    std::cout << input << std::endl;
-    antlr4::ANTLRInputStream instream(input.c_str());
-    LispyLexer lexer(&instream);
-    antlr4::CommonTokenStream tokens(&lexer);
-
-    tokens.fill();
-    for (auto token : tokens.getTokens())
+    while (true)
     {
-        std::cout << token->toString() << std::endl;
-    }
+        std::string input;
+        std::cout << "lispy> ";        // user prompt
+        std::getline(std::cin, input); // get the line from user
+        add_history(input.c_str());
 
-    LispyParser parser(&tokens);
-    antlr4::tree::ParseTree *tree = parser.lispy();
-    std::cout << tree->toStringTree(&parser, true) << std::endl;
-    // }
+        std::cout << input << std::endl;
+        auto instream = new antlr4::ANTLRInputStream(input);
+        auto lexer = new LispyLexer(instream);
+        auto tokens = new antlr4::CommonTokenStream(lexer);
+
+        tokens->fill();
+        /*
+        for (auto token : tokens->getTokens())
+        {
+            std::cout << token->toString() << std::endl;
+        }
+        */
+
+        auto parser = new LispyParser(tokens);
+        antlr4::tree::ParseTree *tree = parser->lispy();
+        std::cout << tree->toStringTree(parser, true) << std::endl;
+    }
 
     return 0;
 }
